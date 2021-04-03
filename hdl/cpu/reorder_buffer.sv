45b0da3a36f37ec75b6361a8f0959bd22d9de52e
Srikar Nalamalapu <svn3@illinois.edu>
2021-04-03 15:11:08 -0500

    broadcasting logic tentatively complete

--- hdl/cpu/reorder_buffer.sv
+++ hdl/cpu/reorder_buffer.sv
@@ -22,9 +22,7 @@ module rob #(
 	output logic load_br_rs,
 	output logic load_alu_rs,
 	output logic load_lsq,
-	output sal_t bcast_br_rs [br_rs_size],
-	output sal_t bcast_alu_rs [alu_rs_size],
-	output sal_t bcast_lsq [lsq_size],
+	output sal_t rob_broadcast_bus [size],
 	output sal_t rdest,
 	output [3:0] rd_tag,
 	output logic reg_ld_instr
@@ -67,6 +65,7 @@ task dequeue():
 	// Check if empty before dequeuing
 	rdest <= sal_t'({ front, arr[front].rdy, arr[front].data });
 	arr[front] <= 0;
+	rob_broadcast_bus[front] <= 0;
 	// dequeued the last one
 	if(front == rear) begin 
 		front <= size;
@@ -85,11 +84,20 @@ task endequeue(logic [width-1:0] data_in):
 	end else begin 
 		out <= arr[front];
 		arr[front] <= 0;
+		rob_broadcast_bus[front] <= 0;
 		front <= (front + 1) % size;
 		rear <= (rear + 1) % size;
 		arr[(rear + 1) % size] <= data_in; 
 	end 
 endtask
+
+task broadcast(logic [3:0] tag):
+	for (int i = 0; i < size; i = i + 1) begin
+		rob_broadcast_bus[i].tag <= tag;
+		rob_broadcast_bus[i].rdy <= 1'b1;
+		rob_broadcast_bus[i].data <= arr[tag].data;
+	end
+endcast
 
 always_comb begin
 	// Enqueue if not full and instr_q is not empty
@@ -112,19 +120,31 @@ always_ff @(posedge clk) begin
 	if(rst) begin
 		front <= -1;
 		rear <= -1;
-		for(int i = 0; i < size; i++) begin 
+		for(int i = 0; i < size; i = i + 1) begin 
 			arr[i] <= 0;
+			rob_broadcast_bus[i] <= 0;
 		end 
+	end else begin
+		if(enq && ~deq) begin
+			enqueue(rob_t'({ instr_q_data, 32'hxxxx, 1'b0, 1'b1 }));
+		end 
+		else if(~enq && deq) begin 
+			dequeue();
+		end 
+		// Necessary?
+		else if(enq && deq) begin 
+			endequeue(rob_t'({ instr_q_data.pc, 32'hxxxx, 1'b0, 1'b1 }));
+		end
+
+		// Update rob entry for incoming completed operation
+		// alu
+		for (int i = 0; i < alu_rs_size; i = i + 1) begin
+			if (alu_rs_o[i].rdy == 1'b1) begin
+				arr[alu_rs_o[i].tag].data <= alu_rs_o[i].data;
+				arr[alu_rs_o[i].tag].rdy <= 1'b1;
+				broadcast(alu_rs_o[i].tag);
+			end
+		end
 	end
-	else if(enq && ~deq) begin
-		enqueue(rob_t'({ instr_q_data, 32'hxxxx, 1'b0, 1'b1 }));
-	end 
-	else if(~enq && deq) begin 
-		dequeue();
-	end 
-	// Necessary?
-	else if(enq && deq) begin 
-		endequeue(rob_t'({ instr_q_data.pc, 32'hxxxx, 1'b0, 1'b1 }));
-	end 
 end
 
