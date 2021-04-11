09273e6afb9e8d1103856e1c546bbed741b4b305
gnodipac886 <ericdong886@yahoo.com>
2021-04-11 14:20:54 -0500

    add testcode

---
+++ testcode/others/mp4_simple_tests.s
@@ -0,0 +1,35 @@
+mp4_simple_tests.s:
+.align 4
+.section .text
+.globl _start
+	# Refer to the RISC-V ISA Spec for the functionality of
+	# the instructions in this test program.
+_start:
+	ori x1, x1, 1
+	ori x1, x1, 2
+	# ori x1, x1, 1
+	# xori x1, x1, 1
+	# addi x1, x1, 1
+	# addi x1, x1, 1
+	# addi x1, x1, 1
+	# addi x1, x1, 1
+	# addi x1, x1, 2
+
+
+halt:			 # Infinite loop to keep the processor
+	beq x0, x0, halt  # from trying to execute the data below.
+					  # Your own programs should also make use
+					  # of an infinite loop at the end.
+
+deadend:
+	lw x8, bad   # X8 <= 0xdeadbeef
+deadloop:
+	beq x8, x8, deadloop
+
+.section .rodata
+
+bad:		.word 0xdeadbeef
+threshold:  .word 0x00000040
+result:	 	.word 0x00000000
+good:	   	.word 0x600d600d
+nice: 		.word 0xb00000b5
