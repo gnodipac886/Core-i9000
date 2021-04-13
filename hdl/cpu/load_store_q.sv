24ad95f43222a75d1d7137c772197f6985c2d556
gnodipac886 <ericdong886@yahoo.com>
2021-04-12 19:21:17 -0500

    fixed lsq bug, need la

--- hdl/cpu/load_store_q.sv
+++ hdl/cpu/load_store_q.sv
@@ -27,13 +27,13 @@ module load_store_q #(
 );
 
 	// internals
-	logic 			front_is_ld;
+	logic 			front_is_ld, front_is_valid, next_front_is_ld, next_front_is_valid;
 	logic 	[1:0] 	remainder;
 	logic 	[31:0] 	shift_amt, mem_address_raw, mem_rdata_shifted;
 
 	// load queue logic
 	logic 			lsq_enq, lsq_deq, lsq_empty, lsq_full, lsq_ready, is_lsq_instr, is_ld_instr, is_st_instr;
-	lsq_t 			lsq_in, lsq_front;
+	lsq_t 			lsq_in, lsq_front, lsq_next_front;
 	lsq_t 			arr[lsq_size];
 	logic 	[31:0]	ld_byte_en;
 
@@ -46,7 +46,8 @@ module load_store_q #(
 	logic 				ready;
 	lsq_t 				out;
 	int 				front, rear;
-
+  	int					next_front, next_rear;
+	  	
 	assign 			enq 				= lsq_enq;
 	assign 			deq 				= lsq_deq;
 	assign 			in 					= lsq_in;
@@ -54,16 +55,19 @@ module load_store_q #(
 	assign 			lsq_full 			= full;
 	assign 			lsq_ready 			= ready;
 	assign 			lsq_front 			= out;
+	assign 			lsq_next_front 		= arr[next_front];
 	assign 			full 				= (front == 0 && rear == size - 1) || (rear == (front - 1) % (size - 1));
 	assign 			empty 				= front == -1;
 	assign			out 				= enq && deq && front == -1 ? in : arr[front];
 
 
 	assign 			front_is_ld 		= lsq_front.pc_info.opcode == op_load;
+	assign 			front_is_valid 		= ~lsq_empty && ~lsq_front.addr_is_tag;		// can be improved, doesn't need to be head
+	assign 			next_front_is_ld 	= lsq_next_front.pc_info.opcode == op_load;
+	assign 			next_front_is_valid = ~lsq_empty && ~lsq_next_front.addr_is_tag;		// can be improved, doesn't need to be head
 	assign 			is_lsq_instr 		= instruction.opcode == op_load || instruction.opcode == op_store;
 	assign 			is_ld_instr 		= instruction.opcode == op_load;
 	assign 			is_st_instr 		= instruction.opcode == op_store;
-	assign 			front_is_valid 		= ~lsq_empty && ~lsq_front.addr_is_tag;		// can be improved, doesn't need to be head
 	assign			lsq_stall			= lsq_full;
 
 	assign 			remainder 			= mem_address_raw[1:0];
@@ -77,6 +81,8 @@ module load_store_q #(
 		ld_byte_en 		= 0;
 		mem_byte_enable = 0;
 		lsq_in 			= '{pc_info: '{opcode: op_imm, default: 0}, default: 0};
+		next_front 		= front;
+		next_rear 		= rear;
 	endfunction : set_default
 
 	task update_q_reg(int i, sal_t rob_item);
@@ -185,6 +191,43 @@ module load_store_q #(
 			sw	: 	mem_byte_enable = 4'b1111;
 			default: ;
 		endcase // store_funct3
+
+		if(enq && ~deq) begin //enqueue
+			if((front == 0 && rear == size - 1) || (rear == (front - 1) % (size - 1))) begin 
+			end 
+			// first element
+			else if(front == -1) begin 
+				next_front 		= 0;
+				next_rear 		= 0;
+			end
+			// otherwise
+			else begin 
+				next_rear 		= (rear + 1) % size;
+			end 
+		end 
+		else if(~enq && deq) begin //dequeue
+			if(front == -1) begin
+			end 
+			else begin
+				// dequeued the last one
+				if(front == rear) begin 
+					next_front 		= -1;
+					next_rear 		= -1;
+				end
+				else begin 
+					next_front 		= (front + 1) % size;
+				end
+			end 
+		end 
+		else if(enq && deq) begin  //endequeue
+			// if empty
+			if(front == -1) begin
+			end 
+			else begin 
+				next_front 			= (front + 1) % size;
+				next_rear 			= (rear + 1) % size;
+			end 
+		end 
 	end 
 
 	always_ff @(posedge clk) begin 
@@ -210,15 +253,15 @@ module load_store_q #(
 			end 
 
 			// see if anything new was posted on rob bus
-			if (rear >= front) begin
-				for (int i = 0; i <= (rear - front) && i < 8; i++)
-					update_q_reg(i + front, rob_bus[arr[i + front].addr[3:0]]);
+			if (next_rear >= next_front) begin
+				for (int i = 0; i <= (next_rear - next_front) && i < 8; i++)
+					update_q_reg(i + next_front, rob_bus[arr[i + next_front].addr[3:0]]);
 			end 
 			else begin 
-				for (int i = 0; i < (size - front + 1) && i < 8; i++)
-					update_q_reg(i + front, rob_bus[arr[i + front].addr[3:0]]);
+				for (int i = 0; i < (size - next_front + 1) && i < 8; i++)
+					update_q_reg(i + next_front, rob_bus[arr[i + next_front].addr[3:0]]);
 		  
-				for (int i = 0; i <= rear && i < 8; i++)
+				for (int i = 0; i <= next_rear && i < 8; i++)
 					update_q_reg(i, rob_bus[arr[i].addr[3:0]]);
 			end
 
@@ -230,18 +273,27 @@ module load_store_q #(
 			// 		update_q_reg(rear, rob_bus[arr[rear].addr[3:0]]);
 
 			if(mem_resp) begin
-				mem_address_raw 	<= lsq_front.addr;
+				mem_address_raw 	<= lsq_next_front.addr; 
+				// we are doing the dequeue of the old instruction, but the front is still the old instruction (only updated next cycle)
+				// we should be looking at the addr of the next valid instruction
+
+				// if (~arr[next_front].addr_is_tag) begin
+				// 	mem_address_raw <= arr[next_front].addr;
+				// end else begin
+				// 	// what is the expected behavior here?
+				// 	mem_address_raw <= 32'bx; // dont read anything?
+				// end
 
 				// read case
 				if(mem_read) begin
 					// arr[front].data 	<= ld_byte_en;
 					lsq_out	 		<= '{tag: lsq_front.rd_tag, rdy: 1'b1, data: ld_byte_en};	// broadcast on read
 				end
-				mem_read 			<= front_is_ld && front_is_valid;
+				mem_read 			<= next_front_is_ld && next_front_is_valid;
 
 				// write case
-				mem_write 			<= ~front_is_ld && front_is_valid;
-				mem_wdata 			<= lsq_front.data;
+				mem_write 			<= ~next_front_is_ld && next_front_is_valid;
+				mem_wdata 			<= lsq_next_front.data;
 				if(mem_write) begin 
 					lsq_out 		<= '{tag: lsq_front.rd_tag, rdy: 1'b1, data: lsq_front.data};
 				end 
@@ -263,116 +315,116 @@ module load_store_q #(
 endmodule : load_store_q
 
 
-module circular_lsq #(parameter width = 32,
-					parameter size 	= 8)
-(
-	input 	logic 				clk,
-	input 	logic 				rst,
-	input 	logic 				enq,
-	input 	logic 				deq,
-	input 	lsq_t 				in,
-	output	logic 				empty,
-	output 	logic 				full,
-	output 	logic 				ready,
-	output 	lsq_t 				out,
-	output 	lsq_t 				arr[size]
-);
+// module circular_lsq #(parameter width = 32,
+// 					parameter size 	= 8)
+// (
+// 	input 	logic 				clk,
+// 	input 	logic 				rst,
+// 	input 	logic 				enq,
+// 	input 	logic 				deq,
+// 	input 	lsq_t 				in,
+// 	output	logic 				empty,
+// 	output 	logic 				full,
+// 	output 	logic 				ready,
+// 	output 	lsq_t 				out,
+// 	output 	lsq_t 				arr[size]
+// );
 
-	int 	front, rear;
+// 	int 	front, rear;
 
-	assign 	full 	= (front == 0 && rear == size - 1) || (rear == (front - 1) % (size - 1));
-	assign 	empty 	= front == -1;
-	assign	out 	= enq && deq && front == -1 ? in : arr[front];
+// 	assign 	full 	= (front == 0 && rear == size - 1) || (rear == (front - 1) % (size - 1));
+// 	assign 	empty 	= front == -1;
+// 	assign	out 	= enq && deq && front == -1 ? in : arr[front];
 
-	task update_q_reg(int i, sal_t rob_item);
-		if(arr[i].addr_is_tag & rob_item.rdy) begin
-			arr[i].addr 		<= rob_item.data + arr[i].pc_info.i_imm;
-			arr[i].addr_is_tag	<= 1'b0;
-		end 
-	endtask
+// 	task update_q_reg(int i, sal_t rob_item);
+// 		if(arr[i].addr_is_tag & rob_item.rdy) begin
+// 			arr[i].addr 		<= rob_item.data + arr[i].pc_info.i_imm;
+// 			arr[i].addr_is_tag	<= 1'b0;
+// 		end 
+// 	endtask
 
-	task enqueue(lsq_t data_in);
-		ready 				<= 0;
-		// full
-		if((front == 0 && rear == size - 1) || (rear == (front - 1) % (size - 1))) begin 
-			return;
-		end 
-		// first element
-		else if(front == -1) begin 
-			front 			<= 0;
-			rear 			<= 0;
-			arr[0]			<= data_in;
-		end
-		// otherwise
-		else begin 
-			rear 					<= (rear + 1) % size;
-			arr[(rear + 1) % size] 	<= data_in; 
-		end 
-	endtask : enqueue
+// 	task enqueue(lsq_t data_in);
+// 		ready 				<= 0;
+// 		// full
+// 		if((front == 0 && rear == size - 1) || (rear == (front - 1) % (size - 1))) begin 
+// 			return;
+// 		end 
+// 		// first element
+// 		else if(front == -1) begin 
+// 			front 			<= 0;
+// 			rear 			<= 0;
+// 			arr[0]			<= data_in;
+// 		end
+// 		// otherwise
+// 		else begin 
+// 			rear 					<= (rear + 1) % size;
+// 			arr[(rear + 1) % size] 	<= data_in; 
+// 		end 
+// 	endtask : enqueue
 
-	task dequeue();
-		// empty
-		if(front == -1) begin
-			ready 				<= 0;
-			return;
-		end 
-		else begin 
-			// out 				<= arr[front];
-			arr[front] 			<= '{default: 0};
-			ready 				<= 1;
-			// dequeued the last one
-			if(front == rear) begin 
-				front 			<= -1;
-				rear 			<= -1;
-			end
-			else begin 
-				front 			<= (front + 1) % size;
-			end
-		end 
-	endtask : dequeue
+// 	task dequeue();
+// 		// empty
+// 		if(front == -1) begin
+// 			ready 				<= 0;
+// 			return;
+// 		end 
+// 		else begin 
+// 			// out 				<= arr[front];
+// 			arr[front] 			<= '{default: 0};
+// 			ready 				<= 1;
+// 			// dequeued the last one
+// 			if(front == rear) begin 
+// 				front 			<= -1;
+// 				rear 			<= -1;
+// 			end
+// 			else begin 
+// 				front 			<= (front + 1) % size;
+// 			end
+// 		end 
+// 	endtask : dequeue
 
-	task endequeue(lsq_t data_in);
-		// if empty
-		if(front == -1) begin 
-			// out 					<= data_in;
-			ready 					<= 0;
-		end 
-		else begin 
-			// out 					<= arr[front];
-			front 					<= (front + 1) % size;
-			rear 					<= (rear + 1) % size;
-			ready 					<= 1;
-			if (~full) begin
-				arr[front] 				<= '{default: 0};
-				arr[(rear + 1) % size] 	<= data_in; 
-			end else begin
-				arr[front]			<= data_in;
-			end
-		end 
-	endtask
+// 	task endequeue(lsq_t data_in);
+// 		// if empty
+// 		if(front == -1) begin 
+// 			// out 					<= data_in;
+// 			ready 					<= 0;
+// 		end 
+// 		else begin 
+// 			// out 					<= arr[front];
+// 			front 					<= (front + 1) % size;
+// 			rear 					<= (rear + 1) % size;
+// 			ready 					<= 1;
+// 			if (~full) begin
+// 				arr[front] 				<= '{default: 0};
+// 				arr[(rear + 1) % size] 	<= data_in; 
+// 			end else begin
+// 				arr[front]			<= data_in;
+// 			end
+// 		end 
+// 	endtask
 
 
-	always_ff @(posedge clk) begin
-		if(rst) begin
-			front 	<= -1;
-			rear 	<= -1;
-			ready 	<= 	0;
-			for(int i = 0; i < size; i++) begin 
-				arr[i] <= '{default: 0};
-			end 
-		end
-		else if(enq && ~deq) begin
-			enqueue(in);
-		end 
-		else if(~enq && deq) begin 
-			dequeue();
-		end 
-		else if(enq && deq) begin 
-			endequeue(in);
-		end 
-	end
+// 	always_ff @(posedge clk) begin
+// 		if(rst) begin
+// 			front 	<= -1;
+// 			rear 	<= -1;
+// 			ready 	<= 	0;
+// 			for(int i = 0; i < size; i++) begin 
+// 				arr[i] <= '{default: 0};
+// 			end 
+// 		end
+// 		else if(enq && ~deq) begin
+// 			enqueue(in);
+// 		end 
+// 		else if(~enq && deq) begin 
+// 			dequeue();
+// 		end 
+// 		else if(enq && deq) begin 
+// 			endequeue(in);
+// 		end 
+// 	end
 
-endmodule : circular_lsq
+// endmodule : circular_lsq
 
 
 // mem_resp		
--- testcode/others/mp4_lsq_tests.s
+++ testcode/others/mp4_lsq_tests.s
@@ -1,28 +1,13 @@
-mp4_simple_tests.s:
+mp4_lsq_tests.s:
 .align 4
 .section .text
 .globl _start
 	# Refer to the RISC-V ISA Spec for the functionality of
 	# the instructions in this test program.
 _start:
-	addi    x0,     x0,     0
-	addi    x2,     x0,     2
-	addi    x4,     x0,     4
-	addi    x6,     x0,     6
-	addi    x8,     x0,     8
-
-alu:
-    add     x4,     x2,     x0      # x4 <= 2 + 0 = 2
-    add     x6,     x8,     x6      # x6 <= 8 + 6 = 14
-    sub     x8,     x2,     x0      # x8 <= 2 - 0 = 2
-    lw      x2,     bad
-    add     x4,     x2,     x8      # dependency on previous instruction
-    la      x5,     result          # x5 <= result
-    sw      x4,     0(x5)           # dependency on add and lw
-    
-
-
-lsq:
+    addi x1,    x1,     1
+    la  x2,     bad
+    sw  x1,     (x2)
 	lw 	x1, 	bad
 	lw 	x2, 	threshold
 	lw 	x3, 	result
